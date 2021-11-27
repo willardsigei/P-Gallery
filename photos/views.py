2dcb3759cf950b25f14b79eddebb381cb89cd1e3
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-28 02:10:33 +0300

    rendered index.html template

--- photos/views.py
+++ photos/views.py
@@ -3,3 +3,9 @@ from django.http  import HttpResponse
 from .models import Image, Category, Location
 
 # Create your views here.
+
+def home(request):
+    images = Image.images()
+    locations = Location.objects.all()
+    return render(request, 'index.html', {'images':images,'locations':locations})
+
