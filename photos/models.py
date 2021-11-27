5f8958eea491062e05e640df2a761412cc24212b
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-27 14:24:15 +0300

    added class image model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    image = CloudinaryField('image')
+    image_location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    image_category = models.ForeignKey('Category',on_delete=models.CASCADE)
